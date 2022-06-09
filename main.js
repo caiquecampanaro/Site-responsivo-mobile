d555a6ce565047f31522046a1c47cea0973e2254
caiquecampanaro <caiquecampanaro@gmail.com>
2022-06-09 15:18:34 -0300

    inclusão dos arquivos para o site

---
+++ main.js
@@ -0,0 +1,15 @@
+function onScroll() {
+    if (scrollY > 0) {
+    navi.classList.add('scroll')
+  } else {
+    navi.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
